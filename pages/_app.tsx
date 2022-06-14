feed1dced9baed61375b008e37963b8f75475da2
wwwwwmw <william.manwai@gmail.com>
2022-06-14 23:40:06 +0800

    supabase, apollo-client  and stepzen added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import  {SessionProvider} from 'next-auth/react'
 import Header from '../components/Header'
-
+import {ApolloProvider} from '@apollo/client'
+import client from '../apollo-client'
 
 
 function MyApp({ 
@@ -10,6 +11,7 @@ function MyApp({
   pageProps: {session, ...pageProps } 
 }: AppProps) {
   return (
+    <ApolloProvider  client={client}>
     <SessionProvider session={session}>
       <div className="h-screen overflow-y-scroll bg-slate-200"> 
         <Header />
@@ -17,6 +19,7 @@ function MyApp({
       </div>
       
     </SessionProvider>
+    </ApolloProvider>
   )
 }
 
