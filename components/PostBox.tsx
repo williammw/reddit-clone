4c08533e5f91a10b9e1a0204f7760e4df30068ea
wwwwwmw <william.manwai@gmail.com>
2022-06-20 00:35:27 +0800

    postbox and avatar with types added

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,15 +1,17 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import Avatar from './Avatar'
 
 function PostBox() {
   const {data:session} = useSession()
   return (
     <form action="">
-      <div>
+      <div className="flex items-center space-x-3">
         {/* Avatar */}
+        <Avatar />
         <input 
           type="text"  
-          className="bg-gray-50 p-2 pl-5 outline-none"
+          className="flex-1 bg-gray-50 p-2 pl-5 outline-none"
           disabled={!session}
           placeholder={
             session? 'Create a post by entering a title!' : 'Sign to Post'
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,8 +6,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     RedditProvider({
-      clientId: "rrT_kkrhtxkxtiHkwR2g8A",
-      clientSecret:"jIzQ0IH1KtlmKNX0qElxjI33ld1dTQ"
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret:process.env.REDDIT_CLIENT_SECRET,
     }),
   ]
 })
